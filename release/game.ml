e6fff125f435b7bb1976e86a76d236f2571afa9d
kmh287 <kmh287@cornell.edu>
2014-04-28 18:03:54 +0000

    Commented the game type and added first-try implementations of game_of state and state_of_game.

--- release/game.ml
+++ release/game.ml
@@ -5,6 +5,7 @@ open Print
 
 
 type game = {
+	(*Board*)
     gHexList                : hex list;
     gPortList               : port list;
     gInterList              : intersection list;
@@ -12,19 +13,67 @@ type game = {
     gDeck                   : deck;
     gDiscard                : discard;
     gRobber                 : robber;
+
+    (*Player list*)
     gPlayerList             : player list;
+
+    (*Turn*)
     gActive                 : color;
     gDiceRolled             : roll option;
     gCardPlayed             : bool;
     gCardsBought            : cards;
     gTradesMade             : int;
     gPendingTrade           : trade option;
+
+    (*Next*)
     gNextColor              : color;
     gNextRequest            : request;
 }
 
-let state_of_game g = failwith "A voice said look me in the stars"
-let game_of_state s = failwith "And tell me truly, men of earth,"
+let state_of_game g = (
+		(*Board*)
+		(g.gHexList,g.gPortList,g.gInterList,g.gRoadList,g.gDeck,
+		g.gDiscard, g.gRobber),
+
+		(*Player List*)
+		gPlayerList,
+
+		(*Turn *)
+		(g.gActive,g.gDiceRolled,g.gCardPlayed,g.gCardsBought
+		g.gTradesMade, g.gPendingTrade),
+
+		(*Next*) 
+		(g.gNextColor, g.gNextRequest)
+		)              
+
+let game_of_state s = match s with 
+	|((hl,pl,il,rl,dk,dc,rb),playerList,(a,dr,cp,cb,tm,pt),(nc,nr))->
+		(*Board*)
+		{gHexList 	= hl;
+		 gPortList 	= pl;
+		 gInterList = il;
+		 gRoadList 	= rl;
+		 gDeck 		= dk;
+		 gDiscard 	= dc; 
+		 gRobber 	= rb; 
+
+		 (*Player List*)
+		 gPlayerList = playerList;
+
+		 (*Turn*)
+		 gActive = a;
+		 gDiceRolled = dr;
+		 gCardPlayed = cp;
+		 gCardsBought = cb;
+		 gTradesMade = tm;
+		 gPendingTrade = pt;
+
+		 (*Next*)
+		 gNextColor = nc;
+		 gNextRequest = nr;
+		 }
+	|_ -> failwith "Implementation fault"
+
 
 
 let init_game () = game_of_state (gen_initial_state())
