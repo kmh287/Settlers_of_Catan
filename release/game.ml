0a1dea8cf932d55abd82afe63e0eb1691f7d9c11
mingxx <mx62@cornell.edu>
2014-04-28 13:33:11 -0400

    update game definition

--- release/game.ml
+++ release/game.ml
@@ -4,7 +4,24 @@ open Util
 open Print
 
 
-type game = Something | Or | Other
+type game = {
+    gHexList                : hex list;
+    gPortList               : port list;
+    gInterList              : intersection list;
+    gRoadList               : road list;
+    gDeck                   : deck;
+    gDiscard                : discard;
+    gRobber                 : robber;
+    gPlayerList             : player list;
+    gActive                 : color;
+    gDiceRolled             : roll option;
+    gCardPlayed             : bool;
+    gCardsBought            : cards;
+    gTradesMade             : int;
+    gPendingTrade           : trade option;
+    gNextColor              : color;
+    gNextRequest            : request;
+}
 
 let state_of_game g = failwith "A voice said look me in the stars"
 let game_of_state s = failwith "And tell me truly, men of earth,"
