cdfc039b5c1d0ed9f17d5333c3e7676e5d3e2534
kmh287 <kmh287@cornell.edu>
2014-04-29 22:47:46 +0000

    Added function stubs for organization. Added in some helper functions. No further progress in handle_move yet.

--- release/game.ml
+++ release/game.ml
@@ -80,6 +80,10 @@ let game_of_state s = match s with
 
 let init_game () = game_of_state (gen_initial_state())
 
+(*Below are the helper functions for the various pieces of handle_move
+None of these handle any specific move type, but rather make calculations
+easier within the functions*) 
+
 (*Function to return a new list with index i 
 replaced with element e with initial list l*)
 let updateList i e l = 
@@ -87,27 +91,76 @@ let updateList i e l =
 		if index = i then e 
 		else listelement) l 
 
+(*Function to search for a point that 
+does not have have a settle men nor
+adjacent settlements*)
+let settleablePoint () = 
+
+	(*Helper function to check if pt && points adjacent to pt are unsettled*)
+	let suitableSettlementPoint pt = 
+		(*Return true if all points in the adjacency list are unsettled*)
+		List.nth gInterList = None && 
+		(List.for_all (fun ele -> List.nth g.gInterList ele = None) 
+						adjacent_points pt) in 
+		
+	(*Find index of first element that is settleable in interlist*)
+	list_indexof suitableSettlementPoint g.gInterList
+
+
 let handle_move s m =
 	let g = game_of_state s in 
 	match m with 
-		|InitialMove( (pt1, pt2) ) -> if nr = InitialRequest
-			(*If this move is of the expected type*) 
-			g with gRoadList 	= (g.gActive,(pt1,pt2))::gRoadList;
-			       gInterList 	=  updateList pt1 (g.gActive,Town) (g.gInterList)
-			       gNextColor	= (*How do we advance this, do not know to go
-			       					in forward or reverse order*)
-				   gNextRequest = (*How can we determine whether the next request
-				   					is an initial move or an action?*) 
-			  
-			else 
-			let unoccupiedPt = list_indexof (fun ele ->
-											if ele = None then true else false)
-											g.gInterList in 
-			let unoccupiedRoad = list_indexof (*Fill in later*)								
-			g with gRoadList 	= 
-				   gInterList 	= updateList unoccupiedPt (g.gActive,Town) (g.gInterList)
-				   gNextColor	= (*See above*)
-				   gNextRequest	= (*See above*)
+		|InitialMove( (pt1, pt2) ) -> 
+				state_of_game (handle_InitialMove g pt1 pt2)  
+		|RobberMove ( (piece,colorOption) ) -> 
+				state_of_game (handle_RobberMove g piece colorOption)
+		|Discard(cost) -> 
+				state_of_game (handle_DiscardMove g cost)
+		|TradeResponse(response) ->
+				state_of_game (handle_TradeResponse g response)
+
+		(*This final case MAY require its own function to match the action*)
+		|Action(action) ->
+				state_of_game (handle_Action g action)
+
+
+
+
+(*************helper functions for each case of handle_move.**********
+****************They are divided into each case***********************
+*****Each function here will take a game and relevent parameters******
+*****and return a game. The conversion to state will happen in *******
+**************************handle_move*********************************)
+
+let handle_InitialMove g pt1 pt2 = 
+	(*If nextRequest is initial move, then handle appropriately*)
+	if g.gNextRequest = InitialMove then begin 
+		g with gRoadList 	= (g.gActive,(pt1,pt2))::gRoadList;
+	       gInterList 	=  updateList pt1 (g.gActive,Town) (g.gInterList)
+	       gNextColor	= (*How do we advance this, do not know to go
+	       					in forward or reverse order*)
+		   gNextRequest = (*How can we determine whether the next request
+		   					is an initial move or an action?*) 
+	  end 
+
+	  (*If nextRequest is not an initial move, then enter a minimal 
+	  move. This move will find the first unoccupied point and settle it*)
+	else begin 
+		let unoccupiedPt = list_indexof (fun ele ->
+										if ele = None then true else false)
+										g.gInterList in 
+		let unoccupiedRoad = list_indexof (*Fill in later*)								
+		g with gRoadList 	= 
+			   gInterList 	= updateList unoccupiedPt (g.gActive,Town) (g.gInterList)
+			   gNextColor	= (*See above*)
+			   gNextRequest	= (*See above*)
+			end 
+
+let handle_RobberMove g piece colorOption = (*STUB*)
+let handle_DiscardMove g cost = (*STUB*)
+let handle_TradeResponse g response = (*STUB*)
+let handle_Action g action = (*STUB*)
+
 
 
 let presentation s = failwith "Were not too much to pay for birth."
